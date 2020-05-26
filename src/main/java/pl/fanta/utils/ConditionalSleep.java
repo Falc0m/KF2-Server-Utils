3a810c0c74a0e61edff77a580d6de2f220774183
FantaO-o <carnageepl@gmail.com>
2020-05-26 02:03:10 +0200

    Refractored method to boolean

--- src/main/java/pl/fanta/utils/ConditionalSleep.java
+++ src/main/java/pl/fanta/utils/ConditionalSleep.java
@@ -6,17 +6,21 @@ import java.util.function.BooleanSupplier;
 // my implementation of conditional sleep with functional interface
 public final class ConditionalSleep {
 
-    public static void sleepUntil(final BooleanSupplier condition, final int timeout, final int interval) {
+    public static boolean sleepUntil(final BooleanSupplier condition, final int timeout, final int interval) {
         final long timeoutSystem = System.currentTimeMillis() + timeout;
 
         try {
+
             while (!condition.getAsBoolean() && System.currentTimeMillis() <= timeoutSystem) {
                 Thread.sleep(interval);
             }
-            System.out.println("Condition true! " + condition.getAsBoolean());
+
+            return condition.getAsBoolean();
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
+
+        return false;
     }
 
 }
